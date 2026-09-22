6cdff6f73345461226e04d78ac8c0616a31e706f
Anvitha <anvithastudy99@gmail.com>
2026-09-22 14:05:24 +0530

    Initial commit

--- src/test/java/com/example/my_maven_project/AppTest.java
+++ src/test/java/com/example/my_maven_project/AppTest.java
@@ -1,7 +1,6 @@
 package com.example.my_maven_project;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,7 +12,20 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testAddition() {
+        assertEquals(5,2+3);
     }
+    @Test
+    public void testSubtraction() {
+        assertEquals(5,8-3);  //FAIL:expected 10,actual 5
+    }
+    @Test
+    public void testMultiplication() {
+        assertEquals(16,4*4); //FAIL:expected 20,actual 16
+    }
+    @Test
+    public void testDivision() {
+        assertEquals(5,10/2);  //PASS
+    }
+    
 }
